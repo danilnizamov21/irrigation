ebde24b10526d81e957a689f769b8c9a797de3ce
levsha_spirit <1evsha3243@gmail.com>
2026-08-09 12:48:32 +0400

    idk

--- api/linking_module_to_user.py
+++ api/linking_module_to_user.py
@@ -10,16 +10,21 @@ from services.linking_module_to_user import LinkinModule
 router = APIRouter()
 
 
+async def get_linking_service(db: AsyncSession = Depends(get_session)) -> LinkinModule:
+    """Зависимость дял получения сервиса LinkinMoudle"""
+    return LinkinModule(db)
+
+
 @router.post("/link_module")
 async def link_module(
     module_key: str,
     token: TokenPayload = Depends(auth.access_token_required),
-    db: AsyncSession = Depends(get_session),
+    service: LinkinModule = Depends(get_linking_service),
 ):
     """Роутер для того чтобы привязать модуль к юзеру"""
-    linking_cls = LinkinModule
+
     user_id = int(token.sub)
-    linking = await linking_cls.linking_module_to_user(module_key, user_id, db)
+    linking = await service.linking_module_to_user(module_key, user_id)
     # по апи ключу найти модуль в бд
     # в линкинг таблицу записать айди модуля и айди юзера
     return linking
@@ -28,22 +33,23 @@ async def link_module(
 @router.get("/get_all_modules")
 async def get_all_model(
     token: TokenPayload = Depends(auth.access_token_required),
-    db: AsyncSession = Depends(get_session),
+    service: LinkinModule = Depends(get_linking_service),
 ):
     """Получение всех id модулей у юзера"""
-    linking_cls = LinkinModule
-    return await linking_cls.get_user_modules(int(token.sub), db)
+
+    user_id = int(token.sub)
+    return await service.get_user_modules(user_id=user_id)
 
 
 @router.get("/get_one_module", response_model=EspResponse)
 async def get_one_module(
     esp_id: int,
     token: TokenPayload = Depends(auth.access_token_required),
-    db: AsyncSession = Depends(get_session),
+    service: LinkinModule = Depends(get_linking_service),
 ):
     """Получние информации по одному модулю"""
-    linking_cls = LinkinModule
-    return await linking_cls.get_module(esp_id, db)
+
+    return await service.get_module(esp_id)
 
 
 @router.patch("/update_module_data")
@@ -51,8 +57,8 @@ async def update_module(
     esp_id: int,
     payload: EspUpdate,
     token: TokenPayload = Depends(auth.access_token_required),
-    db: AsyncSession = Depends(get_session),
+    service: LinkinModule = Depends(get_linking_service),
 ):
     """Обновление данных модуля"""
-    linking_cls = LinkinModule
-    return await linking_cls.update_module(esp_id, payload, db)
+
+    return await service.update_module(esp_id, payload)
